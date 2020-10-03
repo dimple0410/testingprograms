2f5ae042988ae158529b01779bc6e6f429938326
dimpl <dimpl@LAPTOP-J2EAHDLS>
2020-10-03 17:53:57 +0530

    By Dimple Sum Method

--- GITProject/src/github/demo/Demo.java
+++ GITProject/src/github/demo/Demo.java
@@ -2,6 +2,10 @@ package github.demo;
 
 public class Demo {
 
+	public void sum()
+	{
+		System.out.println("In Sum Method....");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello in Main Method");
